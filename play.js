dc66b70eae88c2e6ee901cef42ab69a6b743bbcf
Diego Castro <diegojcr4@gmail.com>
2021-08-25 13:43:23 -0400

    Added functions to handle user input on play.js

--- play.js
+++ play.js
@@ -1,9 +1,26 @@
 const connect = require("./client");
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 // establishes a connection with the game server
-
-
 console.log("Connecting ...");
 connect();
 
