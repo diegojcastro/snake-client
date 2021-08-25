08682caf4b091298a0d82fee76f111408cfefdae
Diego Castro <diegojcr4@gmail.com>
2021-08-25 13:39:16 -0400

    played around with sending move commands

--- client.js
+++ client.js
@@ -13,6 +13,8 @@ const connect = function () {
     console.log("Connection successful.");
     // code that does something when the connection is first established
     conn.write("Name: DJC");
+    
+    
   });
 
   conn.on('data', (data) => {
