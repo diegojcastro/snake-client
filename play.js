81ebbabcaedc11fce373ab04d19200e2fe83c4b3
Diego Castro <diegojcr4@gmail.com>
2021-08-25 13:57:12 -0400

    added WASD movement to input

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
 const connect = require("./client");
+const setupInput = require("./input");
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function () {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 // establishes a connection with the game server
 console.log("Connecting ...");
-connect();
+const conn = connect();
 
+setupInput(conn);
